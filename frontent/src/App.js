54acf333b3771b24722959b0dc491c1495a5045a
913x100 <kittinut.iam@gmail.com>
2021-02-20 11:48:33 +0700

    update

--- frontent/src/App.js
+++ frontent/src/App.js
@@ -1,14 +1,26 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 
+const URL = 'http://localhost:8080/'
+
 function App() {
   const [input, setInput] = useState('');
+  const [isSubmit, setIsSubmit] = useState(false)
   const [list, setList] = useState([]);
   const [username, setUsername] = useState('Guest');
   useEffect(() => {
     const name = window.prompt("Enter a username");
     setUsername(name);
   }, []);
+
+  useEffect(() => {
+    fetch(URL)
+      .then(res => res.json())
+      .then(data => {
+        setList({ name: data.name, message: data.message })
+      })
+
+  }, isSubmit)
 
   const sendMessage = (event) => {
     event.preventDefault();
@@ -18,10 +30,26 @@ function App() {
       message: input,
     };
 
-    setInput("");
+    fetch(URL, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        name: chatMessage.name,
+        message: chatMessage.message,
+      }),
+    }).then((res) => {
+      if (res.status === 200) {
+        setInput("");
+        isSubmit = true
+      }
+    }).catch((error) => {
+
+    })
   };
 
-  console.log(input);
   return (
     <div className="App">
       {list.map(({ id, data: { message, timestamp, name } }) => (
